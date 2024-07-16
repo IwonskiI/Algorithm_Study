4fbcb3fd2e3203ca37a3fc657ccbea78a2d9983f
IwonskiI <ricky2101@naver.com>
2024-07-16 17:56:42 +0900

    BOJ (07/16) - No10845 Temp

--- BOJ/Class2/No10845/src/Main.java
+++ BOJ/Class2/No10845/src/Main.java
@@ -8,7 +8,15 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-
+		int[] queue = new int[10001];
+		int n = Integer.parseInt(br.readLine());
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			String op = st.nextToken();
+			
+		}
+		
+		System.out.println(sb.toString());
 	}
 
 }
