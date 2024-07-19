fa6ffe72d7665c595686181f2d1b7421e233040a
IwonskiI <ricky2101@naver.com>
2024-07-19 17:55:52 +0900

    BOJ (07/19) - No14940 Init

---
+++ BOJ/Class3/No14940/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	
+	// BOJ 14940 - 쉬운 최단거리
+	
+	static int N, M;
+	static int[] dr = {-1, 0, 1, 0}, dc = {0, 1, 0, -1};
+	static int[][] board;
+	static boolean[][] visited;
+	
+	public static void bfs() {
+		
+	}
+	
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		
+
+	}
+
+}
