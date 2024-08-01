209c8cae9ced418900e62b001f34f562d5eeeee4
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-08-01 10:43:22 +0900

    [Bronze V] Title: 구구단, Time: 64 ms, Memory: 11440 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+
+public class Main {
+	
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= 9; i++) {
+			sb.append(n).append(" * ").append(i).append(" = ").append(n*i).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+		
+	}
+
+}
