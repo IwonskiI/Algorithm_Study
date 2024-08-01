c9c3e14c4cda2afeefaf96535d306eca2a28b4e3
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-08-01 10:44:58 +0900

    [Bronze V] Title: N 찍기, Time: 168 ms, Memory: 17172 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+
+public class Main {
+	
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= n; i++) {
+			sb.append(i).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+		
+	}
+
+}
