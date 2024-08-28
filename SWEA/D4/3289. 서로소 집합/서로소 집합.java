e1e4924b5732f8139fb1031748a210b97de07a0c
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-08-28 15:12:21 +0900

    [D4] Title: 서로소 집합, Time: 781 ms, Memory: 115,000 KB -BaekjoonHub

--- SWEA/D4/3289. 서로소 집합/서로소 집합.java
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -5,26 +5,32 @@ public class Solution {
        
     // SWEA 3289 - 서로소 집합
     public static int N, M;
-    public static int[] parents;
+    public static int[] parents, rank;
      
     // union 함수
     public static void union(int x, int y) {
-        // x의 부모 찾기
+        // x, y의 부모 찾기
         x = find(x);
-        // y의 부모 찾기
         y = find(y);
-        // x의 부모가 더 작다면 y의 부모를 x의 부모로 설정
-        if(x < y) parents[y] = x;
-        // y의 부모가 더 작다면 x의 부모를 y의 부모로 설정
-        else parents[x] = y;
+         
+        // 두 부모가 같다면 return
+        if(x == y) return;
+         
+        // 높이가 높은 트리에 높이가 낮은 트리를 붙여줌
+        if(rank[x] > rank[y]) parents[y] = x;
+        else {
+            parents[x] = y;
+            // 높이가 같았다면 한 트리의 루트 노드 하나 아래에 다른 한 트리를 붙여줬기 때문에 최대 높이 증가
+            if(rank[x] == rank[y]) rank[x]++;
+        }
     }
      
     // find 함수
     public static int find(int x) {
         // 현재 값이 루트 노드라면 return
-        if(x == parents[x]) return x;
-        // 아니라면 부모를 탐색
-        else return find(parents[x]);
+        if(parents[x] == x) return x;
+        // 아니라면 부모를 탐색 - path compression
+        else return parents[x] = find(parents[x]);
     }
      
     public static void main(String[] args) throws Exception {
@@ -33,14 +39,16 @@ public class Solution {
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
         for(int tc = 1; tc <= T; tc++) {
-        	sb.append("#").append(tc).append(" ");
+            sb.append("#").append(tc).append(" ");
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             parents = new int[N + 1];
+            rank = new int[N + 1];
             // 부모를 자기 자신으로 초기화
             for(int i = 1; i <= N; i++) {
                 parents[i] = i;
+                rank[i] = 1;
             }
              
             for(int i = 0; i < M; i++) {
