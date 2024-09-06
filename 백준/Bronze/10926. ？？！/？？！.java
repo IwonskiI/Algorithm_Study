e1aeef34758b4e9a457e98743bbb324218f0cadf
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-09-06 21:44:21 +0900

    [Bronze V] Title: ??!, Time: 64 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main{
+    // BOJ 10926 - ??!
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        String id = br.readLine();
+        sb.append(id).append("??!");
+        
+        System.out.println(sb.toString());
+    }
+}
