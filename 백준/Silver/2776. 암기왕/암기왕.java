c0c1cb9d323115da6bc164f5c1ec6b9b7f51ce4d
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-10-04 23:23:42 +0900

    [Silver IV] Title: 암기왕, Time: 1308 ms, Memory: 336100 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -15,13 +15,15 @@ public class Main {
         for(int tc = 0; tc < T; tc++) {
         	// 수첩1에 적혀있는 수의 개수
         	int N = Integer.parseInt(br.readLine());
-        	// 수첩 1에 적혀있는 수의 집합
-        	HashSet<Integer> set = new HashSet<>();
+        	// 수첩 1에 적혀있는 수의 배열
+        	int[] lst = new int[N];
         	// 수 입력
         	st = new StringTokenizer(br.readLine());
         	for(int i = 0; i < N; i++) {
-        		set.add(Integer.parseInt(st.nextToken()));
+        		lst[i] = Integer.parseInt(st.nextToken());
         	}
+        	// 이분 탐색을 위한 정렬
+        	Arrays.sort(lst);
         	
         	// 수첩 2에 적혀있는 수의 개수
         	int M = Integer.parseInt(br.readLine());
@@ -30,10 +32,39 @@ public class Main {
         	for(int i = 0; i < M; i++) {
         		// 수첩 2에 적혀있는 수
         		int cur = Integer.parseInt(st.nextToken());
-        		// 수가 수첩 1에 적혀있던 수라면 1 추가
-        		if(set.contains(cur)) sb.append("1\n");
-        		// 아니라면 0 추가
-        		else sb.append("0\n");
+        		
+        		// 이진 탐색
+        		int start = 0, end = N -1;
+        		// 수를 찾았는지 표시할 flag변수
+        		boolean found = false;
+        		
+        		// 이진탐색 시작
+        		while(start <= end) {
+        			int mid = (start + end)/2;
+        			// 중간 값이 찾는 값이라면
+        			if(lst[mid] == cur) {
+        				// 1 추가 후 종료
+        				sb.append("1\n");
+        				// 수 찾았음을 표시
+        				found = true;
+        				break;
+        			}
+        			
+        			// 찾으려는 수가 중간 값보다 작다면
+        			else if(cur < lst[mid]) {
+        				// 중간 값을 끝점으로 설정하고 재탐색
+        				end = mid - 1;
+        			}
+        			
+        			// 찾으려는 수가 중간 값보다 크다면
+        			else {
+        				// 중간 값을 시작점으로 설정하고 재탐색
+        				start = mid + 1;
+        			}
+        		}
+        		
+        		// 못 찾았다면 0 추가
+        		if(!found) sb.append("0\n");
         	}
         }
         
