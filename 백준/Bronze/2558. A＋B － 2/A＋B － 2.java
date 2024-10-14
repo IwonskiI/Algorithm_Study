8134e8fa122f9b524e02c84afd653c12310f9efb
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-10-14 20:52:49 +0900

    [Bronze V] Title: A+B - 2, Time: 64 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main{
+    
+    //BOJ 2558 - A+B - 2
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // A + B 출력
+        System.out.println(Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine()));
+    }
+}
