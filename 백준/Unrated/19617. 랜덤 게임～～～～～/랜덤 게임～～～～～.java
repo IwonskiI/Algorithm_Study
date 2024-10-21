171972d52f88d11f7c7f6c07c3b432966fd8fd84
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-10-21 19:02:49 +0900

    [Unrated] Title: 랜덤 게임~~~~~, Time: 60 ms, Memory: 11352 KB, Score: 46 point -BaekjoonHub

---
+++ 백준/Unrated/19617. 랜덤 게임～～～～～/랜덤 게임～～～～～.java
@@ -0,0 +1,9 @@
+public class Main {
+    
+    // BOJ 19617 - 랜덤 게임~~~~~
+    
+    public static void main(String[] args){
+    	// 랜덤 숫자 뽑기
+        System.out.println((int)(Math.random() * 2147483647));
+    }
+}
