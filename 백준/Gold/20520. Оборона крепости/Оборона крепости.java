0b3bc05e03539641e942edf45241b13898afb85a
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-10-21 17:45:30 +0900

    [Gold I] Title: Оборона крепости, Time: 1012 ms, Memory: 91560 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/20520. Оборона крепости/Оборона крепости.java
+++ 백준/Gold/20520. Оборона крепости/Оборона крепости.java
@@ -11,46 +11,78 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int S = Integer.parseInt(st.nextToken());
         
-        int ans = 0;
-        // 한 명을 배치했을 때 가장 많은 사람을 없앨 수 있는 순서대로 정렬되는 PQ
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)-> {
-        	int asum, bsum;
-        	if(a[0] >= a[1]) asum = a[1];
-        	else asum = a[0];
-        	if(b[0] >= b[1]) bsum = b[1];
-        	else bsum = b[0];
-        	return bsum - asum;
-        });
+        long ans = 0;
+        
+        long[][] lst = new long[500001][2];
+        
+        int idx = 0;
+        HashMap<Long, Integer> map = new HashMap<>();
+        
         // 공격자의 수와 공격가능한 사람의 수를 입력
         for(int i = 0; i < N; i++) {
         	st = new StringTokenizer(br.readLine());
+        	// 공격자 수
         	int attack = Integer.parseInt(st.nextToken());
+        	// 방어자 한 사람이 처치할 수 있는 공격자 수
         	int defend = Integer.parseInt(st.nextToken());
+        	// 이번 구역에서 최대로 공격자를 잡을 수 있는 방어자의 수
+        	int full = attack / defend;
+        	// 이번에 잡을 수 있는 최대 공격자 수를 이전에 공격한 적이 있다면
+        	if(map.containsKey(defend)) {
+        		// 이번 방어자 수만 추가
+        		lst[map.get(defend)][0] += full;
+        	}
+        	// 공격한 적이 없다면
+        	else {
+        		// 현재 인덱스에 현재 공격자 수를 등록
+        		map.put(Integer.toUnsignedLong(defend), idx);
+        		// 인덱스 저장 및 방어자 수 추가
+        		lst[idx][1] = defend;
+        		lst[idx++][0] += full;
+        		
+        	}
+        	// 나머지 계산
+        	int remain = attack % defend;
+        	// 나머지가 있다면 위와 같은 방법으로 추가
+        	if(remain != 0) {
+            	if(map.containsKey(remain)) {
+            		lst[map.get(remain)][0] += 1;
+            	}
+            	else {
+            		map.put(Integer.toUnsignedLong(remain), idx);
+            		lst[idx][1] = remain;
+            		lst[idx++][0] += 1;
+            	}
+        	}
         	// 총 공격 인원수 증가
         	ans += attack;
-        	// pq 에 추가
-        	pq.offer(new int[] {attack, defend});
         }
         
-        // 모든 사람을 배치할 때까지 진행
-        for(int i = 0; i < S; i++) {
-        	// 만약 이미 모든 공격자를 처리했으면 종료
-        	if(ans == 0) break;
-        	// 현재 가장 많이 제거할 수 있는 구역 출력
-        	int[] cur = pq.poll();
-        	// 이번 사람이 제거하는 인원을 제거한 후 남은 사람 저장
-        	int left = cur[1] > cur[0] ? 0 : cur[0] - cur[1];
-        	// 이번 턴에 제거될 사람의 수 계산
-        	int remove;
-        	if(cur[0] >= cur[1]) remove = cur[1];
-        	else remove = cur[0];
-        	// 전체 공격자 수 중 이번 턴에 제거 되는 사람 제거
-        	ans -= remove;
-        	// 제거되고 남은 구역의 공격자를 pq에 추가
-        	pq.offer(new int[] {left, cur[1]});
+        // 가장 많이 없앨 수 있게 정렬
+        Arrays.sort(lst, (a, b) -> (int)(b[1] - a[1]));
+        
+        // 가장 많이 없앨 수 있는 사람부터 삭제
+        idx = 0;
+        // 방어자가 남아있다면 계속 진행
+        while(S > 0) {
+        	// 처치해야할 적이 0이면 종료
+        	if(ans <= 0) break;
+        	// 이번 인덱스에 처치해야하는데 필요한 인원수가 현재 인원수 보다 많다면
+        	if(lst[idx][0] > S) {
+        		// 남아있는 적 수에서 현재 인원수만큼만 투입해서 적 제거
+        		ans -= (lst[idx][1] * S);
+        		// 인원수 모두 소진
+        		S = 0;
+        	}
+        	// 아니라면
+        	else {
+        		// 현재 인덱스의 처리해야하는데 필요한 인원수를 모두 투입해서 적 제거
+        		ans -= (lst[idx][1] * lst[idx][0]);
+        		// 이번 인덱스에 투입된 인원수 감소 후 인덱스 증가
+        		S -= lst[idx++][0];
+        	}
         }
-        
-        
+
         // 남은 공격자 수 출력
         System.out.println(ans);        
     }
