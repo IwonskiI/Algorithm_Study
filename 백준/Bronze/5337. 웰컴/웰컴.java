d8d3c684ee6c20d953ec721f97a29170485790e9
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-11-08 23:55:27 +0900

    [Bronze V] Title: 웰컴, Time: 60 ms, Memory: 11236 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,11 @@
+public class Main {
+	// BOJ 5337 - 웰컴
+	public static void main(String[] args) {
+		
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+		
+	}
+
+}
