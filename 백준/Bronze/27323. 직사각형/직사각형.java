b7a8bdb380bc85c415cb66d894bce3d6a312a1ee
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-11-13 15:11:02 +0900

    [Bronze V] Title: 직사각형, Time: 68 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    
+    // BOJ 27233 - 직사각형
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // 직사각형의 넓이 계산
+        System.out.println(Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()));
+    }
+}
