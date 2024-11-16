55f8d79c2de1afa91326c581064a048c470c1c1c
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-11-17 02:33:53 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 80 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+
+	// BOJ 2743 - 단어 길이 재기
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// 단어 길이 출력
+		System.out.println(br.readLine().length());
+	}
+
+}
