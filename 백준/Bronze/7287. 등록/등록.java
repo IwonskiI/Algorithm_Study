561d7a9fdde32f6992f641fda2f9eff2368c8973
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-11-19 23:39:12 +0900

    [Bronze V] Title: 등록, Time: 60 ms, Memory: 11252 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	// BOJ 7287 - 등록
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		
+		// 맞힌 문제 수와 아이디 저장
+		sb.append(313).append("\n").append("wonski1202");
+		
+		// 최종 결과 출력
+		System.out.println(sb.toString());
+	}
+
+}
