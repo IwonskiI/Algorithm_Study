7cbf414929ec84aadb9fb71740861ed277c6bf51
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-11-23 00:16:24 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 172 ms, Memory: 15096 KB -BaekjoonHub

--- 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -8,13 +8,15 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		
 		// 큰 수 두개 입력
 		BigInteger a = new BigInteger(st.nextToken());
 		BigInteger b = new BigInteger(st.nextToken());
+		sb.append(a.add(b));
 		
 		// 최종 결과 저장
-		System.out.println(a.add(b));
+		System.out.println(sb.toString());
 	}
 
 }
