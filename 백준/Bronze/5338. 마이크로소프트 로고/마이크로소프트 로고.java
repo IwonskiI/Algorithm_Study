497c7198d4453a526330261a287ce292508d09af
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-11-25 19:16:55 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 60 ms, Memory: 11216 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,15 @@
+public class Main {
+    
+    // BOJ 5338 - 마이크로소프트 로고
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("       _.-;;-._\n");
+        sb.append("'-..-'|   ||   |\n");
+        sb.append("'-..-'|_.-;;-._|\n");
+        sb.append("'-..-'|   ||   |\n");
+        sb.append("'-..-'|_.-''-._|\n");
+        
+        System.out.println(sb.toString());
+    }
+}
