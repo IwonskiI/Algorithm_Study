56ba9b8594cc0a208f6b89031ed94d8490438f4d
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-11-29 20:41:13 +0900

    [Bronze V] Title: 콜센터, Time: 60 ms, Memory: 11236 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,21 @@
+public class Main {
+	
+	// BOJ 5339 - 콜센터
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("     /~\\\n");
+		sb.append("    ( oo|\n");
+		sb.append("    _\\=/_\n");
+		sb.append("   /  _  \\\n");
+		sb.append("  //|/.\\|\\\\\n");
+		sb.append(" ||  \\ /  ||\n");
+		sb.append("============\n");
+		sb.append("|          |\n");
+		sb.append("|          |\n");
+		sb.append("|          |\n");
+
+		System.out.println(sb.toString());
+	}
+
+}
