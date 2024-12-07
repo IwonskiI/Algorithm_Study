94f3b46278c7647af9ef4040ff928b8166e9bbc6
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-12-07 23:10:23 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 64 ms, Memory: 11556 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+	
+	// BOJ 24262 - 알고리즘 수업 - 알고리즘의 수행 시간 1
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		Integer.parseInt(br.readLine());
+		
+		sb.append("1\n0");
+		
+		System.out.println(sb.toString());
+	}
+
+}
