393c3deb3f989e56b8afbbb874c563d7acffbf1b
Wonski <86334960+IwonskiI@users.noreply.github.com>
2024-12-22 16:58:33 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 64 ms, Memory: 11256 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,9 @@
+public class Main {
+    
+    // BOJ 16170 - 오늘의 날짜는?
+    public static void main(String[] args) {
+        // 오늘 날짜 출력
+        System.out.println("2024\n12\n22");
+    }
+    
+}
