27c7574f5d1baa4e880949a5d3523611dc947c39
Wonski <86334960+IwonskiI@users.noreply.github.com>
2025-01-06 23:47:22 +0900

    [Bronze V] Title: The World Responds, Time: 68 ms, Memory: 11228 KB -BaekjoonHub

---
+++ 백준/Bronze/23234. The World Responds/The World Responds.java
@@ -0,0 +1,11 @@
+public class Main {
+    
+    // BOJ 23234 - The World Responds
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("The world says hello!");
+        
+        System.out.println(sb.toString());
+    }
+}
