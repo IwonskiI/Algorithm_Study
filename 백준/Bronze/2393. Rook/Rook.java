07e4af48697191319b7aedc8fdd79dd25d8fd55d
Wonski <86334960+IwonskiI@users.noreply.github.com>
2025-01-07 14:32:54 +0900

    [Bronze V] Title: Rook, Time: 64 ms, Memory: 11388 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,23 @@
+public class Main {
+    
+    // BOJ 2393 - Rook
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("  ___  ___  ___\n" +
+                  "  | |__| |__| |\n" +
+                  "  |           |\n" +
+                  "   \\_________/\n" +
+                  "    \\_______/\n" +
+                  "     |     |\n" +
+                  "     |     |\n" +
+                  "     |     |\n" +
+                  "     |     |\n" +
+                  "     |_____|\n" +
+                  "  __/       \\__\n" +
+                  " /             \\\n" +
+                  "/_______________\\\n");
+        
+        System.out.println(sb.toString());
+    }
+}
