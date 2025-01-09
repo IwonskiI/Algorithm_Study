c85e4a5658534a787437a063a1aee6cbd21fbe4f
Wonski <86334960+IwonskiI@users.noreply.github.com>
2025-01-09 22:02:36 +0900

    [Bronze V] Title: Hook, Time: 60 ms, Memory: 11252 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,14 @@
+public class Main {
+    
+    // BOJ 10189 - Hook
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("#  # #### #### #  #\n" +
+                  "#### #  # #  # # #\n" +
+                  "#### #  # #  # # #\n" +
+                  "#  # #### #### #  #");
+        
+        System.out.println(sb.toString());
+    }
+}
