a48f2aa06340731cfb000e714952d8f04413afde
Wonski <86334960+IwonskiI@users.noreply.github.com>
2025-03-22 01:31:32 +0900

    [Silver IV] Title: 피하자, Time: 424 ms, Memory: 163340 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/25379. 피하자/피하자.java
+++ 백준/Silver/25379. 피하자/피하자.java
@@ -1,39 +1,29 @@
-import java.io.*;
+	import java.io.*;
 import java.util.*;
-
+ 
 public class Main {
-    
-    // BOJ 25379 - 피하자
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int[] lst = new int[N];
-        int start_odd = -1, start_even = -1;
-        int last_odd = -1, last_even = -1;
-        for(int i = 0; i < N; i++){
-            lst[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        int odd_ans = 0, even_ans = 0;
-        int odd_cnt = 0, even_cnt = 0;
-        for(int i = 0; i < N; i++){
-            // 짝수 계산
-            if(lst[i] % 2 == 0) {
-                even_ans += (i - even_cnt);
-                even_cnt++;
-            }
-            else{
-                odd_ans += (i - odd_cnt);
-                odd_cnt++;
+        StringTokenizer st;
+		
+        int N = Integer.parseInt(br.readLine()); // 개수
+		
+        long Lcnt = 0;
+        long Rcnt = 0;
+        long sum = 0;
+        int idx = 0;
+		
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+			
+            if (num % 2 == 0) {
+                sum += idx++;
+                Lcnt += i;
+                Rcnt += N - 1 - i;
             }
         }
         
-        int ans = even_ans < odd_ans ? even_ans : odd_ans;
-        
-        System.out.println(ans);
-        
+        System.out.println(Math.min(Lcnt, Rcnt) - sum);
     }
 }
